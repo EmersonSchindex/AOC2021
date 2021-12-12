1f06b2c1569dbeebb859771f68e0c1c2eb9f8ef8
EmersonSchindex <gvans63@gmail.com>
2021-12-12 18:56:24 +0100

    Solution for Day 2 part 2

--- day10/day1001.py
+++ day10/day1001.py
@@ -1,8 +1,11 @@
+import math
+
 def main(f):
     with open(f, "r") as file1:
         lines = [(line.strip()).split('|') for line in file1 if line.strip()]
 
-    print(part1(lines))
+#    print(part1(lines))
+    print(part2(lines))
 
 def part1(lines):
     errorscore = 0
@@ -39,8 +42,58 @@ def check_balance(expression):
                 closestack.pop()
             else:
                 closestack.append(c)
+    
     return closestack
 
+def check_balance2(expression):
+    openlist = ['(', '[', '{', '<']
+    closelist = [')', ']', '}', '>']
+    points = [1, 2, 3, 4]
+    score = 0
+    openstack = []
+    closestack = []
+    for c in expression:
+        if c in openlist:
+            openstack.append(c)
+        elif c in closelist:
+            closestack.append(c)
+            pos = closelist.index(c)
+            if len(openstack) > 0 and openstack[-1] == openlist[pos]:
+                openstack.pop()
+                closestack.pop()
+            else:
+                closestack.append(c)
+    
+    if len(closestack) == 0:
+        openstack.reverse()
+        for c in openstack:
+            pos = openlist.index(c)
+            score = (score * 5) + points[pos]
+
+    return score
+
+def part2(lines):
+    totalscore = []
+    totalscore2 = []
+    newlines = []
+    balance = 0
+    finalscore = 0
+    for l in range(len(lines)):
+        stack = []
+        regel = str(lines[l])
+        regel = regel[2:len(regel)-2]
+        balance = check_balance2(regel)
+        if balance > 0:
+            totalscore.append(balance)
+
+    totalscore2 = sorted(totalscore)
+    midvalue = math.floor(len(totalscore2) / 2)
+    finalscore = totalscore2[midvalue]
+
+#    print(midvalue)
+
+#    print(finalscore)
+    return finalscore
 
 if __name__ == "__main__":
     main('input.txt')
