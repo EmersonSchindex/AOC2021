ec15cf1990289ca58512133fe9c2840374c6c76e
EmersonSchindex <gvans63@gmail.com>
2021-12-12 22:01:57 +0100

    Update Day 12 part 1

--- day12/day1201.py
+++ day12/day1201.py
@@ -5,17 +5,17 @@ def main(f):
     print(part1(lines))
 
 def part1(lines):
-    errorscore = 0
 
-    nodes = {}
+    graph = {}
     
     for l in range(len(lines)):
         teststring = str(lines[l])
         sleutel = teststring[2 : int(teststring.index('-'))]
         waarde = teststring[int(teststring.index('-')) + 1 : len(teststring) - 2]
-        nodes.setdefault(sleutel, set()).add(waarde)
+        graph.setdefault(sleutel, list()).append(waarde)
+        graph.setdefault(waarde, list()).append(sleutel)
 
-    return errorscore
+    return graph
 
 def part2(lines):
     finalscore = 0
